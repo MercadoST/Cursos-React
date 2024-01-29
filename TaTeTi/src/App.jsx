88563bd289f7300d0d9f85d126d8d6f8055e1dc6
MercadoST <santi_merca@live.com.ar>
2024-01-28 21:17:39 -0300

    Save to storage

--- TaTeTi/src/App.jsx
+++ TaTeTi/src/App.jsx
@@ -6,11 +6,19 @@ import { TURNS } from "./constants"
 import { WinnerModal } from "./components/WinnerModal"
 import { checkEndGame, checkWinnerFrom } from "./logic/board"
 import { TurnSection } from "./components/TurnSection"
+import { resetGameStorage, saveGameStorage } from "./logic/storage"
 
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return (boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null))
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  });
   const [winner, setWinner] = useState(null) // null = sin ganador, false = empate
 
   const updateBoard = (index) => {
@@ -24,13 +32,17 @@ function App() {
     //cambiar el turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //guardar aqui partida
+    saveGameStorage({
+      board: newBoard,
+      turn: newTurn,
+  })
     //revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       conffetti()
       setWinner(newWinner)
-    } else if (checkEndGame(newBoard))
-    {
+    } else if (checkEndGame(newBoard)) {
       setWinner(false)
     }
   }
@@ -39,6 +51,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage();
   }
 
   return (
@@ -60,7 +74,7 @@ function App() {
           })
         }
       </section>
-        <TurnSection turn={turn} />
+      <TurnSection turn={turn} />
       <section>
         <WinnerModal winner={winner} resetGame={resetGame}>{winner}</WinnerModal>
       </section>
---
+++ TaTeTi/src/logic/storage/index.js
@@ -0,0 +1,11 @@
+export const resetGameStorage = () => {
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
+}
+
+export const saveGameStorage = ({ board, turn }) => {
+
+  window.localStorage.setItem('board', JSON.stringify(board))
+  window.localStorage.setItem('turn', turn)
+
+}
